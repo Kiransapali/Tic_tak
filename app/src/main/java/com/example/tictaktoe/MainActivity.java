166e429de18b4378dfadda29039d45a2c84b445d
Kiran Sapali <kriancsapali@gmail.com>
2024-02-25 18:07:31 +0530

    added todo

--- app/src/main/java/com/example/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/tictaktoe/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     int activePlayer=0;
     int[] gameState= {2,2,2,2,2,2,2,2,2};
+
+    //TODO @kiran
     int [][] winPossion={{0,1,2,},{3,4,5,},{6,7,8},
             {0,3,6},{1,4,7},{2,5,8},
             {0,4,8},{2,4,6}};
